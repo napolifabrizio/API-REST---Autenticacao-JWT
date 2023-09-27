6eb2a071dc056feb17d8212a98f72928bf01febe
Fabrizio <fabrizionapoli50@gmail.com>
2023-09-27 10:48:33 -0300

    desenvolvendoAPI - implementando cadastro usuário

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({msg: "Bem vindo a nossa API"});
-})
+app.use(express.json());
+
+const User = require("./models/User");
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Bem vindo a nossa API" });
+});
+
+app.post('/auth/register', async(req, res) => {
+    const {name, email, password, confirmPassword} = req.body;
+
+    if (!name) return res.status(422).json({msg: 'Nome inválido'});
+    if (!email) return res.status(422).json({msg: 'Email inválido'});
+    if (!password) return res.status(422).json({msg: 'Senha inválida'});
+    if (confirmPassword != password) return res.status(422).json({msg: 'As senhas não batem'});
+});
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluester0.hde5qpu.mongodb.net/`).then(() => {
+mongoose
+  .connect(`mongodb+srv://${dbUser}:${dbPass}@cluester0.hde5qpu.mongodb.net/`)
+  .then(() => {
     app.listen(3000);
-    console.log("Conectou ao banco")
-}).catch((err) => console.log(err));
-
-
+    console.log("Conectou ao banco");
+  })
+  .catch((err) => console.log(err));
