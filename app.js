e7253010236ce415f31ec3fca2bc2af776d68331
Fabrizio <fabrizionapoli50@gmail.com>
2023-09-29 11:21:44 -0300

    desenvovlendo API - cadastro usuário feito + login em desenvolv

--- app.js
+++ app.js
@@ -14,13 +14,43 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo a nossa API" });
 });
 
-app.post('/auth/register', async(req, res) => {
-    const {name, email, password, confirmPassword} = req.body;
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
 
-    if (!name) return res.status(422).json({msg: 'Nome inválido'});
-    if (!email) return res.status(422).json({msg: 'Email inválido'});
-    if (!password) return res.status(422).json({msg: 'Senha inválida'});
-    if (confirmPassword != password) return res.status(422).json({msg: 'As senhas não batem'});
+  if (!name) return res.status(422).json({ msg: "Nome inválido" });
+  if (!email) return res.status(422).json({ msg: "Email inválido" });
+  if (!password) return res.status(422).json({ msg: "Senha inválida" });
+  if (confirmPassword != password)
+    return res.status(422).json({ msg: "As senhas não batem" });
+
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    return res.status(422).json({ msg: "Esse email já possui um cadastro" });
+
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+    res.status(201).json({ msg: "Usuário cadastrado!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Ocorreu um erro no servidor" });
+  }
+});
+
+app.post("auth/login", async (req, res) => {
+  const {password, email} = req.body;
+
+  if (!email) return res.status(422).json({ msg: "Email inválido" });
+  if (!password) return res.status(422).json({ msg: "Senha inválida" });
+
 });
 
 const dbUser = process.env.DB_USER;
